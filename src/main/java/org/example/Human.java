875c580da112ccb56080e5100a6309101b84cfe6
Kaminski Aleksandr <testEmail@gmail.com>
2025-05-21 19:35:25 +0300

    Добавил конструктор в файла Human.java

--- src/main/java/org/example/Human.java
+++ src/main/java/org/example/Human.java
@@ -3,4 +3,9 @@ package org.example;
 public class Human {
     private String name;
     private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
